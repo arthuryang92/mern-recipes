f550f5ac2baedd3fba808581b28bbda80e1b65a6
unknown <arthuryangzeng@hotmail.com>
2023-07-25 18:10:44 +0800

    2nd push

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Home} from "./pages/home";
+import {Auth} from "./pages/auth";
+import {useCookies} from 'react-cookie'
+import {CreateRecipe} from "./pages/create-recipe";
+import {SavedRecipes} from "./pages/saved-recipes";
+import { Navbar } from './components/navbar';
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/auth' element={<Auth/>}></Route>
+          <Route path='/create-recipe' element={<CreateRecipe/>}></Route>
+          <Route path='/saved-recipes' element={<SavedRecipes/>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
