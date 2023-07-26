705d427c6d21fd23b6d29d5678d794ef56ed3701
unknown <arthuryangzeng@hotmail.com>
2023-07-26 17:49:54 +0800

    push 3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,13 @@
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import {useCookies} from 'react-cookie'
-import {useNavigate} from 'react-router-dom'
 export const Navbar = () =>{
 
     const [cookies, setCookies] = useCookies(["access_token"])
     const navigate = useNavigate()
     const logout = () =>{
-        setCookies("accessToken", "")
-        window.localStorage.removeItem("userID")
-        navigate("/auth");
+        setCookies("access_token", "");
+    window.localStorage.clear();
+    navigate("/auth");
     }
     return <div className="navbar">
         <Link to="/">Home</Link>
